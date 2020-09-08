8e3050467d78c4fe1547e59409c865f30f3a81f6
Filip Johansson <filip.johansson@protonmail.com>
2020-09-08 19:53:18 +0200

    refactor: move initialState to separate file

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,11 @@
 import React, { useReducer } from "react";
-import "./styles/index.css";
 import reducer from "./store/reducer";
 import { wrapperStyle } from "./styles/styles";
 import FileList from "./components/FileList";
 import useFileUpload from "./hooks/useFileUpload";
 import useFileUploadBatchControl from "./hooks/useFileUploadBatchControl";
 import FileUploadForm from "./components/FileUploadForm";
-
-const initialState = {
-  files: [],
-  fileStatusArray: [],
-  fileResponseData: [],
-};
+import initialState from "./store/initialState";
 
 const FileUpload = ({ url, callback, requestBatchSize = 1 }) => {
   const [state, dispatch] = useReducer(reducer, {
