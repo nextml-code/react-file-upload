75c75a41563127a60371d70cc105484e8ac9a117
Filip Johansson <filip.johansson@protonmail.com>
2021-03-19 12:12:33 +0100

    feature: out of the box styling

--- src/core/events.js
+++ src/core/events.js
@@ -1,6 +1,4 @@
 import { START_DRAG, STOP_DRAG } from "../store/actionTypes";
-import { map, identity } from "./functional";
-import appendFiles from "../store/appendFiles";
 
 export const handleOnDragEnter = ({ dispatch }) => {
   dispatch({ type: START_DRAG });
@@ -8,30 +6,6 @@ export const handleOnDragEnter = ({ dispatch }) => {
 
 export const handleOnDragExit = ({ dispatch }) => {
   dispatch({ type: STOP_DRAG });
-};
-
-const getFileList = (dataTransfer) => {
-  if (dataTransfer.items) {
-    return map([...dataTransfer.items], (file) => {
-      if (file.kind === "file") {
-        return file.getAsFile();
-      }
-    });
-  } else {
-    return map([...dataTransfer.files], identity);
-  }
-};
-
-export const handleOnDrop = ({ event, dispatch }) => {
-  event.preventDefault();
-  const files = getFileList(event.dataTransfer);
-  dispatch(appendFiles(files));
-};
-
-export const handleOnChange = ({ dispatch, event }) => {
-  event.preventDefault();
-  const { files } = event.target;
-  dispatch(appendFiles(files));
 };
 
 export const preventEventDefault = (event) => event.preventDefault();
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,29 +1,4 @@
 /**
- * Style for the form both when
- * dragging and not dragging
- */
-const commonStyle = {
-  display: "flex",
-  flexDirection: "column",
-  fontFamily: "var(--aiwizo-application-default-font)",
-  backgroundColor: "var(--aiwizo-application-light-background-blue)",
-  alignItems: "center",
-  fontWeight: 900,
-  fontSize: "var(--aiwizo-application-font-size-big)",
-  color: "#111",
-  borderTopRightRadius: "var(--aiwizo-application-border-radius-primary)",
-  borderTopLeftRadius: "var(--aiwizo-application-border-radius-primary)",
-};
-
-/**
- * Style for the form when dragging
- */
-const draggingStyle = {
-  backgroundColor: "var(--aiwizo-application-blue)",
-  color: "#ffffff",
-};
-
-/**
  *
  */
 export const wrapperStyle = {
