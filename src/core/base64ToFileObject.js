251c6e7c8c9732b44c9b5866d19c8d1cb19b0c9b
Filip Johansson <filip.johansson@protonmail.com>
2021-05-07 16:38:18 +0200

    feature: better metadata

--- src/core/base64ToFileObject.js
+++
@@ -1,8 +0,0 @@
-const base64ToFileObject = (base64String) => {
-  const [metadata, base64] = base64String.split(",");
-  const [format] = metadata.split(":")[1].split(";");
-
-  return { data: base64, format };
-};
-
-export default base64ToFileObject;
