ffd702bdd53063de33e65afc6e715c6d518a7cc7
Filip Johansson <filip@aiwizo.com>
2023-11-21 15:30:25 +0100

    storybook update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,18 @@
 module.exports = {
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
-  framework: "@storybook/react",
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
+  }
 };
